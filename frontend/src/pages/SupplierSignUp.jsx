1b679d07d00efeeedd0f2664a3ee1ed1ecd7c6c5
BKRavindu <bkravindu2000@gmai.com>
2024-07-02 21:25:58 +0530

    frontend for signup

---
+++ frontend/src/pages/SupplierSignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SupplierSignUp from "../components/SupplierSignUp.jsx";
+
+const SupplierSignUpPage = () => {
+  return (
+    <div>
+        <SupplierSignUp />
+    </div>
+  )
+}
+
+export default SupplierSignUpPage;
