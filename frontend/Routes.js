297d049284e320d4a8ff4670035d28c8681ef98b
BKRavindu <bkravindu2000@gmai.com>
2024-07-02 21:16:30 +0530

    add routes

---
+++ frontend/Routes.js
@@ -0,0 +1,10 @@
+import LoginPage from "../src/pages/Login";
+import SignUpPage from "../src/pages/Signup";
+import SupplierSignUpPage from "../src/pages/SupplierSignUp"
+
+export{
+    LoginPage,
+    SignUpPage,
+    SupplierSignUpPage,
+    
+}
