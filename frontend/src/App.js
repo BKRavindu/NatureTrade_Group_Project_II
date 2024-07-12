cae825dbf69d5ae5306dff1a502cb314635fffc6
GitAvi001 <avindusridharmawardhana@gmail.com>
2024-07-12 12:19:17 +0530

    commit supplier pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import DeliveryDash from './pages/deliveryCompany/DeliveryDash';
 import OrderView from './pages/deliveryCompany/OrderView';
 import Order from './pages/deliveryCompany/Order';
 import CollectorDash from './pages/CollectingCenter/CollectorDash';
+import SupplierDash from './pages/supplier/SupplierDash.jsx';
+import Requests from './pages/supplier/Requests.jsx';
+import Store from './pages/supplier/Store.jsx'
+
 
 
 function App() {
@@ -19,6 +23,10 @@ function App() {
         <Route path="/OrderView" element={<OrderView />} />
         <Route path="/Order" element={<Order />} />
         <Route path="/CollectorDash" element={<CollectorDash/>}/>
+        <Route path="/SupplierDash" element={<SupplierDash/>}/>
+        <Route path="/Requests" element={<Requests/>}/>
+        <Route path="/Store" element={<Store/>}/>
+      
       </Routes>
     </BrowserRouter>
   );
