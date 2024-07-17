985ead9c1971382b61e92f4d35d0786d5bdc3c85
GitAvi001 <avindusridharmawardhana@gmail.com>
2024-07-17 21:53:53 +0530

    changes for the supplier sidebar and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {LoginPage, SignUpPage} from "./Routes.js";
-import SupplierSignUp from './components/SupplierSignUp.jsx';
+// import {LoginPage, SignUpPage} from "./Routes.js";
+// import SupplierSignUp from './components/SupplierSignUp.jsx';
 import DeliveryDash from './pages/deliveryCompany/DeliveryDash';
 import OrderView from './pages/deliveryCompany/OrderView';
 import Order from './pages/deliveryCompany/Order';
 import Subscription from './pages/deliveryCompany/Subscription';
 import SubscriptionActive from './pages/deliveryCompany/SubscriptionActive';
 import CollectorDash from './pages/CollectingCenter/CollectorDash';
+import CollectorStore from './pages/CollectingCenter/CollectorStore';
 import BuyerDash from './pages/Buyer/BuyerDash';
 import BOrders from './pages/Buyer/BOrders';
 import BCollectingcenter from './pages/Buyer/BCollectingcenter';
@@ -19,16 +20,19 @@ import Contact from './pages/admin/Contact';
 import Centers from './pages/admin/Centers';
 import SupplierDash from './pages/supplier/SupplierDash.jsx';
 import Requests from './pages/supplier/Requests.jsx';
-import Store from './pages/supplier/Store.jsx'
-import CollectorStore from './pages/CollectingCenter/CollectorStore';
+import SupplierStore from './pages/supplier/SupplierStore.jsx'
+import Advertisments from './pages/supplier/Advertisments.jsx';
+import SupplierContact from './pages/supplier/SupplierContact.jsx'
+import SupplierProfile from './pages/supplier/SupplierProfile.jsx'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage /> } />
-        <Route path="/sign-up" element={<SignUpPage /> } />
-        <Route path="/supplier-sign-up" element={<SupplierSignUp />} />
+        {/* <Route path="/login" element={<LoginPage /> } /> */}
+        {/* <Route path="/sign-up" element={<SignUpPage /> } />
+        <Route path="/supplier-sign-up" element={<SupplierSignUp />} /> */}
         <Route path="/Delivery/Dashboard" element={<DeliveryDash />} />
         <Route path="/Delivery/OrderView" element={<OrderView />} />
         <Route path="/Delivery/Order" element={<Order />} />
@@ -40,13 +44,17 @@ function App() {
         <Route path="/Admin/Contact" element={<Contact/>}/>
         <Route path="/Admin/Centers" element={<Centers/>}/>
         <Route path="/CollectorDash" element={<CollectorDash/>}/>
+        <Route path="/CollectorStore" element={<CollectorStore/>}/>
         <Route path="/BuyerDash" element={<BuyerDash/>}/>
         <Route path="/BOrders" element={<BOrders/>}/>
         <Route path="/BCollectingcenter" element={<BCollectingcenter/>}/>
         <Route path="/BDelivery" element={<BDelivery/>}/>
-        <Route path="/SupplierDash" element={<SupplierDash/>}/>
-        <Route path="/Requests" element={<Requests/>}/>
-        <Route path="/Store" element={<Store/>}/>
+        <Route path="/supplier/Dashboard" element={<SupplierDash/>}/>
+        <Route path="/supplier/Requests" element={<Requests/>}/>
+        <Route path="/supplier/SupplierStore" element={<SupplierStore/>}/>
+        <Route path="/supplier/Advertisments" element={<Advertisments/>}/>
+        <Route path="/supplier/SupplierContact" element={<SupplierContact/>}/>
+        <Route path="/supplier/SupplierProfile" element={<SupplierProfile/>}/>
       </Routes>
     </BrowserRouter>
     
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,10 +1,10 @@
-import LoginPage from "../src/pages/Login";
-import SignUpPage from "../src/pages/Signup";
+// import LoginPage from "../src/pages/LoginPage";
+// import SignUpPage from "../src/pages/SignupPage";
 import SupplierSignUpPage from "../src/pages/SupplierSignUp"
 
 export{
-    LoginPage,
-    SignUpPage,
+    // LoginPage,
+    // SignUpPage,
     SupplierSignUpPage,
     
 }
