f666fd2d509cc9ac6cee4b59c939894a9b1ceb91
Iflal <iflalm5@gmail.com>
2024-07-24 22:46:15 +0530

    admin sidebar & navbar component created

--- frontend/src/pages/admin/AdminDash.jsx
+++ frontend/src/pages/admin/AdminDash.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../../components/admin/NavBar";
+import SideBar from "../../components/admin/SideBar";
 
 export default function AdminDash() {
   const tr = {};
   return (
     <>
       <NavBar />
+      <SideBar/>
       <div>AdminDash</div>
     </>
   );
