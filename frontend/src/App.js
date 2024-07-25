341fdd50db73b4f526fb52de8ce16b9b5948a889
BKRavindu <bkravindu2000@gmail.com>
2024-07-25 22:56:40 +0530

    styling on signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
