26616f3d0981243630a7d856f99dc9be568b22f5
Viruli Vimansa Weerasinghe <2021cs214@stu.ucsc.cmb.ac.lk>
2024-07-29 16:31:43 +0530

    collecting center

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,7 +3,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import styles from '../styles/styles';
 import {Link} from 'react-router-dom';
 import {RxAvatar} from 'react-icons/rx';
-import { server } from "../../server";
+import { server } from "../server";
 import axios from "axios";
 
 const SignUp = () => {
--- frontend/src/pages/ActivationPage.js
+++ frontend/src/pages/ActivationPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from "axios";
-import { server } from '../../server.js';
+import { server } from '../server.js';
 
 const ActivationPage = () => {
   const { activation_token } = useParams();
