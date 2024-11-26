ae5a062501dbde98c3da10dda2ee373a6b51583f
BKRavindu <bkravindu2000@gmail.com>
2024-11-26 08:27:39 +0530

    new updates

--- frontend/src/Routes.js
+++
@@ -1,9 +0,0 @@
-import Login from "../src/pages/Login/Login";
-import Signup from "../src/pages/Signup/Signup";
-import ActivationPage from "../src/pages/ActivationPage"
-
-export{
-    Login,
-    Signup,
-    ActivationPage,
-}
